fe7262f3f7583ac88a3223dac471422447b49b55
Prisha Sharma <psharma1_be22@thapar.edu>
2025-04-21 19:00:38 +0530

    ..

--- app/components/SolarSystem3d.tsx
+++ app/components/SolarSystem3d.tsx
@@ -1,9 +1,8 @@
 'use client'
 
-import React, { useRef, useState, useEffect } from 'react'
+import React, { useRef } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { OrbitControls, Stars, Html } from '@react-three/drei'
-import { useRouter } from 'next/navigation'
 import * as THREE from 'three'
 
 type PlanetProps = {
